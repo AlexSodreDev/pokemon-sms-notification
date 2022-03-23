4c856704b86470206ebd337c9585f6e59a23775e
AlexSodreDev <alexandre.cortines@gmail.com>
2022-03-23 10:10:18 -0300

    add .env and command to send sms

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import AWS, { SQS } from 'aws-sdk'
+import initMB from 'messagebird'
+import { config } from 'dotenv'
+import util from 'util'
 
 (async function main () {
+  config()
   while (true) {
     try {
       const queueUrl = 'here you place the url of queue in AWS SQS'
@@ -20,6 +24,22 @@ import AWS, { SQS } from 'aws-sdk'
           ReceiptHandle: message.ReceiptHandle
         }).promise()
       })
+
+      const messageBird = initMB(process.env.MESSAGE_BIRD_API_KEY_TEST)
+
+      const send = util.promisify(messageBird.messages.create)
+
+      const smsParams = {
+        originator: 'TestMessage',
+        recipients: [
+          process.env.SMS_PHONE_RECIPIENTS
+        ],
+        body: 'This is a test message - Have Faith'
+      }
+
+      const smsResult = await send(smsParams)
+
+      console.log(smsResult)
     } catch (err) {
       console.log(err)
     }
